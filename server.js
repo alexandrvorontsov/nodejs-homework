9772a814e1ef4d53cffbd4e9811cf6bbb6649c98
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2023-12-20 04:58:06 +0200

    fix .env

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST } = process.env;
+const app = require("./app");
+// const { error } = require("./schemas/contact");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database conect"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
