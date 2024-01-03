8359e6280996f008e6eb402c1416e0a2ce1ceea0
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2024-01-03 09:06:44 +0200

    add token

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -18,7 +18,7 @@ const login = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   await User.findByIdAndUpdate(user._id, { token });
   res.json({
-    data: { token },
+    token,
     user: { email, subscription },
   });
 };
