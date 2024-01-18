ec35a0fce618dff55495f55fa2a7b877821170b1
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2024-01-18 14:48:59 +0200

    fix

--- controllers/users/resendVerifyEmail.js
+++ controllers/users/resendVerifyEmail.js
@@ -16,7 +16,7 @@ const resendVerifyEmail = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: "Verify email",
-    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Click to confirm your email</a>`,
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click to confirm your email</a>`,
   };
 
   await sendEmail(verifyEmail);
